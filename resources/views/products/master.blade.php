64d3e7ce428a0a11513e237cbef1aed36411fccd
oestupinang <oestupinang@unipanamericana.edu.co>
2018-09-17 00:06:12 -0500

    Product CRUD

---
+++ resources/views/products/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Productos</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css">
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
